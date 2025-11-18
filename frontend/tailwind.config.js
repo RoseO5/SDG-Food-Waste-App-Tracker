e398dfa2cdb705813f592e306f75f2f2d3c53f14
Rose05 <talktorose90@gmail.com>
2025-11-18 01:30:33 +0100

    Fix Next.js structure + working build

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,12 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
