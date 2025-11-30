ebcd480934405b45b21ddd2b93173dd7a029891a
RoseO5 <talktorose90@gmail.com>
2025-11-30 21:30:40 +0100

    fix: restore TypeScript and simplify mongodb client

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -5,21 +5,5 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI;
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
-if (process.env.NODE_ENV === 'development') {
-  const globalWithMongo = global as typeof global & {
-    _mongoClientPromise?: Promise<MongoClient>;
-  };
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri);
-    globalWithMongo._mongoClientPromise = client.connect();
-  }
-  clientPromise = globalWithMongo._mongoClientPromise;
-} else {
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
-}
-
-export default clientPromise;
+const client = new MongoClient(uri);
+export default client;
